12aed1dfe31580aca329ef35beca7bf8b174cfbc
Gabi <gabrielabguedes@gmail.com>
2020-09-21 20:27:48 -0300

    Add walking sound on Part IV. Add jumping and colision sounds on Part IV. Remove unecessary sound assets.
    Signed-off-by: Gabi <gabrielabguedes@gmail.com>

--- workshop/Assets/Part IV/Scripts/PlayerMovimentation.cs
+++ workshop/Assets/Part IV/Scripts/PlayerMovimentation.cs
@@ -11,11 +11,26 @@ namespace PartIV
 		public float maxSpeed = 5;
 		public float jumpSpeed = 5;
 
-		// Private attributes
-		Rigidbody2D rigidBody;
+        //Player Sound
+		private AudioSource source;
+		public AudioClip stepsSound, jumpSound, colisionSound;
+
+		private bool colidedWithEnemy = false;
+
+
+        // Private attributes
+        Rigidbody2D rigidBody;
 		bool isJumping = false;
 
 		private PlayerHealth health;
+
+		void setSound(AudioClip sound, Boolean isLoop)
+		{
+            source.Stop();
+            source.clip = sound;
+            source.loop = isLoop;
+            source.Play();
+		}
 
 		// Start is called before the first frame update
 		// Here you can handle with all objects in the scene
@@ -29,6 +44,10 @@ namespace PartIV
 		{
 			rigidBody = GetComponent<Rigidbody2D>();
 			health = GetComponent<PlayerHealth>();
+
+			// Player sounds
+            source = GetComponent<AudioSource>();
+            setSound(stepsSound, true);
 		}
 
 		// Update is called once per frame
@@ -48,16 +67,47 @@ namespace PartIV
 			{
 				isJumping = true;
 				rigidBody.velocity = new Vector2(rigidBody.velocity.x, jump * jumpSpeed);
-			}
+
+                //Handle jump sound
+                setSound(jumpSound, false);
+            }
+
+			// Handle steps sound only while walking
+            if (x != 0 && !isJumping)
+            {
+				if(colidedWithEnemy && !source.isPlaying)
+				{
+					colidedWithEnemy = false;
+					setSound(stepsSound, true);
+				}
+				else
+				{
+                	source.UnPause();
+				}
+            }
+            else if (!isJumping)
+            {
+                source.Pause();
+            }
 		}
 
 		// OnCollisionEnter2D is called whenever the player hits something
 		void OnCollisionEnter2D(Collision2D other)
 		{
-			Debug.Log("Collision with: " + other.gameObject.name);
-			if (other.contacts[0].normal == Vector2.up)
+			// Debug.Log("Collision with: " + other.gameObject.name);
+			if (other.contacts[0].normal == Vector2.up && isJumping)
 			{
 				isJumping = false;
+
+				// Set sound to steps after jump
+                setSound(stepsSound, true);
+            }
+
+			// Set sound on enemy colision
+            if (other.gameObject.CompareTag("Enemy"))
+            {
+				colidedWithEnemy = true;
+				setSound(colisionSound, false);
 			}
 		}
 	}
